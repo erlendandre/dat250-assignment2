2bda37b8b6d949334dfdbfe4a30465711f60bffe
erlendandre <erlendandre@icloud.com>
2025-08-26 10:51:41 +0200

    Add welcome text

--- src/main/java/no/hvl/dat250/assignment1/Assignment1Application.java
+++ src/main/java/no/hvl/dat250/assignment1/Assignment1Application.java
@@ -2,12 +2,17 @@ package no.hvl.dat250.assignment1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Assignment1Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Assignment1Application.class, args);
-	}
-
+    public static void main(String[] args) {
+      SpringApplication.run(Assignment1Application.class, args);
+    }
+    @GetMapping("/")
+    public String rootGreeting() {
+        return "Successful demo for DAT250 🚀";
+    }
 }
